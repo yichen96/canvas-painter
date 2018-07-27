efb043ca91cdceef54a4296bc6d4e0ce4ca80ea7
Yichen Liu <liuyichenpanda@gmail.com>
2018-07-27 18:36:25 +0100

    update diagonal line test

--- src/test/java/com/testproject/PainterTest.java
+++ src/test/java/com/testproject/PainterTest.java
@@ -1,6 +1,7 @@
 package com.testproject;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -12,7 +13,7 @@ public class PainterTest {
         painter.prepareCanvas(20,5);
     }
 
-    @org.junit.Test
+    @Test
     public void prepareCanvas() {
         //assert size of canvas
         assertEquals(7,painter.getCanvas().length);
@@ -24,7 +25,7 @@ public class PainterTest {
         assertEquals('|', painter.getCanvas()[1][0]);
     }
 
-    @org.junit.Test
+    @Test
     public void drawLineOnCanvas() {
         painter.drawLineOnCanvas(1,3,7,3);
         assertEquals('x',painter.getCanvas()[3][2]);
@@ -32,11 +33,16 @@ public class PainterTest {
         assertEquals('|',painter.getCanvas()[3][0]);
     }
 
-    @org.junit.Test
+    @Test
     public void drawRectangleOnCanvas() {
         painter.drawRectangleOnCanvas(15,2,20,5);
         assertEquals('x',painter.getCanvas()[3][20]);
         //inside rectangle is blank
         assertEquals(' ',painter.getCanvas()[3][19]);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void drawDiagonalFail(){
+        painter.drawLineOnCanvas(1,3,7,2);
     }
 }
